593efb5f2b61976e80a3fdfbb034d851b71e51e6
ljk <ssobbs13@gmail.com>
2024-12-29 21:49:53 +0900

    init

---
+++ src/test/java/com/example/was1/Was1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.was1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Was1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
